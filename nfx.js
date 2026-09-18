8e8c33479adca527b6eaa35277f1b8f870b7b5d9
Nfxgroup <emmanuelchukwuma996@gmail.com>
2026-09-18 13:05:59 +0100

    initial NFX WORLD website

---
+++ nfx.js
@@ -0,0 +1,9 @@
+ function myfunction () 
+ {
+  document.getElementById('demo').innerHTML = Date();
+    }
+     
+let lastname = "NFX GROUP";
+  let lastName = "Nazzyfx";
+    document.getElementById('nf').innerHTML= lastname;
+   
